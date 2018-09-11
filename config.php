72f8b7cbd2b8b9dabc824dfeb8150bc9a3aa51ab
filosoff13 <filosoff13@users.noreply.github.com>
2018-09-11 23:32:07 +0300

    first
    new

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php  
+    //параметры для соединения с БД
+return [
+    'host' => 'localhost',
+    'dbname'=> 'easy',
+    'user' => 'root',
+    'password' => '',
+    'charset' => 'utf8'
+    
+    ];
+
+?> 
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+use Dev\Database;
+
+$config = require_once 'config.php';
+//print_r($config);
+
+$db = new Database($config);
+
+$query = $db->query('SELECT requests.id, offers.name, requests.price, requests.count count, operators.name op_name FROM requests LEFT JOIN offers ON requests.offer_id = offers.id LEFT JOIN operators ON requests.operator_id = operators.id WHERE count > 2 AND requests.operator_id in (10, 12)');
+print_r($query);
+
+// $db->closeConnection();
+?>
+<table border="1">
+    <p>Первый вариант</p>
+    <tr>
+        <th> Номер заказа </th>
+        <th> Имя товара </th>
+        <th> Цена </th>
+        <th> Количество </th>
+        <th> Имя оператора </th>
+    </tr>
+
+    <?php foreach ($query as $value) {?>
+        <tr>
+            <th> <?=$value['id']?> </th>
+            <th> <?=$value['name']?> </th>
+            <th> <?=$value['price']?> </th>
+            <th> <?=$value['count']?> </th>
+            <th> <?=$value['op_name']?> </th>
+        </tr>
+    <?php } ?>
+</table>
+
+<?php
+$query = $db->query('SELECT SUM(requests.count) count, SUM(requests.price) price, offers.name name FROM requests LEFT JOIN offers ON offers.id = requests.offer_id GROUP BY name');
+// print_r($query);
+
+$db->closeConnection();
+?>
+<table border="1">
+    <p>Второй вариант</p>
+    <tr>
+        <th> Имя товара </th>
+        <th> Количество товара </th>
+        <th> Сумма </th>
+    </tr>
+
+    <?php foreach ($query as $value) {?>
+        <tr>
+            <th> <?=$value['name']?> </th>
+            <th> <?=$value['count']?> </th>
+            <th> <?=$value['price']?> </th>
+        </tr>
+    <?php } ?>
